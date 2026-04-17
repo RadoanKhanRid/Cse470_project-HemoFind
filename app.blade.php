860d20f2e0e4bb5ac0f1042eb211d31f0219dec8
Sinawath Siam <sinawathsiam55@gmail.com>
2026-04-17 19:36:25 +0600

    Update app.blade.php

--- app.blade.php
+++ app.blade.php
@@ -7,11 +7,11 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
+     
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
+
         <script src="https://cdn.tailwindcss.com"></script>
         <script>
             tailwind.config = {
@@ -36,7 +36,7 @@
         <div class="min-h-screen bg-red-50">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
+
             @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -45,7 +45,7 @@
                 </header>
             @endisset
 
-            <!-- Page Content -->
+
             <main>
                 {{ $slot }}
             </main>
