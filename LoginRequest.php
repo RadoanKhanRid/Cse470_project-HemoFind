fa67c84fbc71590fc91028633e624c909a3636f2
Sinawath Siam <sinawathsiam55@gmail.com>
2026-04-17 19:29:32 +0600

    Update LoginRequest.php

--- LoginRequest.php
+++ LoginRequest.php
@@ -20,11 +20,6 @@ class LoginRequest extends FormRequest
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
